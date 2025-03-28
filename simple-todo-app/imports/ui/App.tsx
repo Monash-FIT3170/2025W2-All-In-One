e05a3252e6a89eeedf461c73f32cfeaeb83cbac3
Shen-SLJ <shen.jng1@gmail.com>
2025-03-29 03:51:07 +1100

    add tsconfig json

--- simple-todo-app/imports/ui/App.tsx
+++ simple-todo-app/imports/ui/App.tsx
@@ -1,22 +1,17 @@
 import React from "react";
-import { Task } from "./Task.tsx";
+import { Task } from "./Task";
 import { useTracker, useSubscribe } from "meteor/react-meteor-data";
-import { TasksCollection } from "../api/TasksCollection.ts";
+import { TasksCollection } from "../api/TasksCollection";
 
+// Would usually be centralised in a data model definition
 type Task = {
   _id: number;
   text: string;
 };
 
-const tasks: Array<Task> = [
-  { _id: 1, text: "First Task" },
-  { _id: 2, text: "Second Task" },
-  { _id: 3, text: "Third Task" },
-];
-
 export const App = () => {
   const isLoading = useSubscribe("tasks");
-  const tasks = useTracker(() => TasksCollection.find({}).fetch());
+  const tasks: Array<Task> = useTracker(() => TasksCollection.find({}).fetch());
 
   if (isLoading()) {
     return <div>Loading...</div>;
