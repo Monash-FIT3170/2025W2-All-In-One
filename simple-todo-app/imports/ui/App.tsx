62ab50413d8de3b690e50ea4f10e8dcc417d8388
Shen-SLJ <shen.jng1@gmail.com>
2025-03-29 02:59:38 +1100

    linking up with a local mongodb instance

--- simple-todo-app/imports/ui/App.tsx
+++ simple-todo-app/imports/ui/App.tsx
@@ -1,22 +1,35 @@
-import React from 'react';
-import { Task } from './Task.tsx';
+import React from "react";
+import { Task } from "./Task.tsx";
+import { useTracker, useSubscribe } from "meteor/react-meteor-data";
+import { TasksCollection } from "../api/TasksCollection.ts";
 
 type Task = {
   _id: number;
   text: string;
-}
+};
 
 const tasks: Array<Task> = [
-  {_id: 1, text: 'First Task'},
-  {_id: 2, text: 'Second Task'},
-  {_id: 3, text: 'Third Task'},
+  { _id: 1, text: "First Task" },
+  { _id: 2, text: "Second Task" },
+  { _id: 3, text: "Third Task" },
 ];
 
-export const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
-    <ul>
-      { tasks.map((task) => <Task key={task._id} task={task} />) }
-    </ul>
-  </div>
-);
+export const App = () => {
+  const isLoading = useSubscribe("tasks");
+  const tasks = useTracker(() => TasksCollection.find({}).fetch());
+
+  if (isLoading()) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <div>
+        <h1>Welcome to Meteor!</h1>
+        <ul>
+          {tasks.map((task) => (
+            <Task key={task._id} task={task} />
+          ))}
+        </ul>
+      </div>
+    );
+  }
+};
