8f6f9959b76d3d89f4e9ebb35f0c4de2841529b3
Shen-SLJ <shen.jng1@gmail.com>
2025-03-29 03:00:48 +1100

    fix: stuck on loading due to not publishing

--- simple-todo-app/server/main.js
+++ simple-todo-app/server/main.js
@@ -1,5 +1,6 @@
 import { Meteor } from "meteor/meteor";
 import { TasksCollection } from "/imports/api/TasksCollection";
+import "../imports/api/TaskPublications"; 
 
 const insertTask = (taskText) =>
   TasksCollection.insertAsync({ text: taskText });
