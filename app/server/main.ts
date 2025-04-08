14821f2f67e8d11902a24bcc6762c8406418f3a3
Shen-SLJ <shen.jng1@gmail.com>
2025-04-09 01:33:57 +1000

    refactored seeding function out of meteor.startup

--- app/server/main.ts
+++ app/server/main.ts
@@ -1,11 +1,11 @@
 import { Meteor } from "meteor/meteor";
 import { TasksCollection } from "/library-modules/apis/example-tasks/task-collection";
-import "/library-modules/apis/example-tasks/publications/task-publications"; 
+import "/library-modules/apis/example-tasks/publications/task-publications";
 
-const insertTask = (taskText: string) =>
-  TasksCollection.insertAsync({ text: taskText });
+Meteor.startup(tempSeedFunction);
 
-Meteor.startup(async () => {
+// TODO: This code and below is temporary and will be removed in the future.
+async function tempSeedFunction(): Promise<void> {
   if ((await TasksCollection.find().countAsync()) === 0) {
     [
       "First Task",
@@ -17,4 +17,7 @@ Meteor.startup(async () => {
       "Seventh Task",
     ].forEach(insertTask);
   }
-});
+}
+
+const insertTask = (taskText: string) =>
+  TasksCollection.insertAsync({ text: taskText });
