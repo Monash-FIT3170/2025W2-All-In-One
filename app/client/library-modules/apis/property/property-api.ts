aff70478646cb0060112d0a69889262843515033
b-onbon <sonahariharan067@gmail.com>
2025-05-27 22:32:44 +1000

    86cymwcnm - wrap landlord methods into functions in property-api.ts - as per feedback, wrapped the 5 landlord methods required to interact with db

--- app/client/library-modules/apis/property/property-api.ts
+++ app/client/library-modules/apis/property/property-api.ts
@@ -2,26 +2,91 @@ import { PropertyStatus } from "/app/shared/api-models/property/PropertyStatus";
 import { ApiProperty } from "/app/shared/api-models/property/ApiProperty";
 import { MeteorMethodIdentifier } from "/app/shared/meteor-method-identifier";
 import { PropertyInsertData } from "/app/shared/api-models/property/PropertyInsertData";
-import { Property } from "../../domain-models/property/Property";
 import { PropertyUpdateData } from "/app/shared/api-models/property/PropertyUpdateData";
 
 export async function apiGetPropertyById(id: string): Promise<ApiProperty> {
-  const fetchedProperty = await Meteor.callAsync(MeteorMethodIdentifier.PROPERTY_GET, id);
+  const fetchedProperty = await Meteor.callAsync(
+    MeteorMethodIdentifier.PROPERTY_GET,
+    id
+  );
 
   return fetchedProperty;
 }
 
-export async function apiGetPropertyStatusId(name: PropertyStatus): Promise<string> {
-  return await Meteor.callAsync(MeteorMethodIdentifier.PROPERTY_STATUS_GET, name);
+export async function apiGetPropertyStatusId(
+  name: PropertyStatus
+): Promise<string> {
+  return await Meteor.callAsync(
+    MeteorMethodIdentifier.PROPERTY_STATUS_GET,
+    name
+  );
 }
 
-export async function apiInsertProperty(property: PropertyInsertData): Promise<string> {
-  return await Meteor.callAsync(MeteorMethodIdentifier.PROPERTY_INSERT, property);
+export async function apiInsertProperty(
+  property: PropertyInsertData
+): Promise<string> {
+  return await Meteor.callAsync(
+    MeteorMethodIdentifier.PROPERTY_INSERT,
+    property
+  );
 }
 
-export async function apiUpdatePropertyData(updatedProperty: PropertyUpdateData): Promise<string> {
-  return await Meteor.callAsync(MeteorMethodIdentifier.PROPERTY_DATA_UPDATE, updatedProperty);
-}export async function apiGetAllProperties(): Promise<ApiProperty[]> {
-  const fetchedProperties: ApiProperty[] = await Meteor.callAsync(MeteorMethodIdentifier.PROPERTY_GET_ALL);
+export async function apiUpdatePropertyData(
+  updatedProperty: PropertyUpdateData
+): Promise<string> {
+  return await Meteor.callAsync(
+    MeteorMethodIdentifier.PROPERTY_DATA_UPDATE,
+    updatedProperty
+  );
+}
+export async function apiGetAllProperties(): Promise<ApiProperty[]> {
+  const fetchedProperties: ApiProperty[] = await Meteor.callAsync(
+    MeteorMethodIdentifier.PROPERTY_GET_ALL
+  );
   return fetchedProperties;
+}
+
+export async function apiGetLandlordPropertyCount(
+  landlordId: string
+): Promise<number> {
+  return await Meteor.callAsync(
+    MeteorMethodIdentifier.PROPERTY_LANDLORD_GET_COUNT,
+    landlordId
+  );
+}
+
+export async function apiGetLandlordStatusCounts(
+  landlordId: string
+): Promise<{ occupied: number; vacant: number }> {
+  return await Meteor.callAsync(
+    MeteorMethodIdentifier.PROPERTY_LANDLORD_GET_STATUS_COUNTS,
+    landlordId
+  );
+}
+
+export async function apiGetLandlordIncome(
+  landlordId: string
+): Promise<{ weekly: number; monthly: number }> {
+  return await Meteor.callAsync(
+    MeteorMethodIdentifier.PROPERTY_LANDLORD_GET_TOTAL_INCOME,
+    landlordId
+  );
+}
+
+export async function apiGetLandlordOccupancyRate(
+  landlordId: string
+): Promise<number> {
+  return await Meteor.callAsync(
+    MeteorMethodIdentifier.PROPERTY_LANDLORD_GET_OCCUPANCY_RATE,
+    landlordId
+  );
+}
+
+export async function apiGetLandlordAverageRent(
+  landlordId: string
+): Promise<{ occupiedCount: number; rent: number }> {
+  return await Meteor.callAsync(
+    MeteorMethodIdentifier.PROPERTY_LANDLORD_GET_AVERAGE_RENT,
+    landlordId
+  );
 }
